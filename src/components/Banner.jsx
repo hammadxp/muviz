4c343506af86b7f546c2f76a32b31a7852a385ba
hammadxp <hammadjaved9860@gmail.com>
2023-06-23 22:45:13 +0500

    banner main image without using background-image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,16 +14,18 @@ export default function Banner() {
 
   return (
     <div className="mb-8 flex w-full gap-6" id="banner">
-      <div className="h-96 w-2/3">
-        <div
-          className={`h-full bg-[linear-gradient(to_top,rgba(0,0,0,0.3),rgba(0,0,0,0)),url('${
-            backdropBaseURL + results?.[0].backdrop_path
-          }')] bg-cover bg-center bg-no-repeat`}
-        >
-          <div className="bottom-0 left-0">
+      <div className="relative h-96 w-2/3">
+        <img
+          src={backdropBaseURL + results?.[0].backdrop_path}
+          alt={results?.[0].name + "backdrop"}
+          className="h-full w-full rounded-xl object-cover shadow-2xl transition "
+        />
+
+        <div className="absolute bottom-0 left-0 w-full rounded-xl bg-gradient-to-t from-black to-transparent p-12">
+          <div className="flex max-w-md flex-col gap-6">
             <p>{results?.[0].overview}</p>
 
-            <div className="flex gap-2">
+            <div className="flex gap-[12px]">
               <button className="flex items-center gap-2 rounded-md bg-slate-50 py-[6px] pl-6 pr-7 text-slate-900">
                 <HiOutlinePlay />
                 Play
@@ -35,11 +37,6 @@ export default function Banner() {
             </div>
           </div>
         </div>
-        {/* <img
-          src={backdropBaseURL + results?.[0].backdrop_path}
-          alt={results?.[0].name + "backdrop"}
-          className="h-full w-full rounded-xl object-cover shadow-2xl transition hover:scale-[.99]"
-        /> */}
       </div>
       <div className="flex h-96 w-1/3 flex-col gap-6">
         <img
